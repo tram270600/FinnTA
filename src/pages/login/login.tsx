4315602ee09fe706e2fb78225845409199fab17b
= <=>
2021-05-30 16:21:05 +0700

    [Update] Login Page + Fix bug

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,5 +1,5 @@
-import React, {SyntheticEvent, useState} from 'react';
-import {Redirect} from 'react-router-dom'; 
+import React, { SyntheticEvent, useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import '../../styles/login.scss'
 
 import showPw from '../../images/showPw.svg';
@@ -11,49 +11,49 @@ const Login = () => {
     const [redirect, setRedirect] = useState(false);
     const [showPassword, setShowPassword] = useState(false);
 
-    const handleForm = async(e:SyntheticEvent)=>{
+    const handleForm = async (e: SyntheticEvent) => {
         e.preventDefault();
-        await talker.login({Email, Password})
+        await talker.login({ Email, Password })
         setRedirect(true);
     }
 
-    if(redirect) return <Redirect to="/"/>
+    if (redirect) return <Redirect to="/" />
 
-    return ( 
-    <div className="loginForm"> 
-        <div className="header">
-            <h1>Connect with other<br/>Make you better</h1>
-            <a href="/"><b>Sign in</b> to your FinCon Account !</a>
-        </div> 
-        <form className="inputForm" onSubmit = {handleForm} method = "POST"> 
-            <div className="field">
-                <input
-                    type="text"
-                    name="ipEmail"
-                    id="ipEmail"
-                    placeholder="avc@avc.com"
-                    onChange={(e) => {
-                    setEmail(e.target.value)
-                }} required/>
-                <label>Username/Email</label>
-            </div> 
-            <div className = "field">
-                <input
-                type={showPassword ? "text" : "password"}
-                name="ipPass"
-                id="ipPass"
-                placeholder="ThisIsMyPassword"
-                onChange={(e) => {setPw(e.target.value)}} required/>
-                <label> Password </label>
-                <img src={showPw} width={24} onClick={()=>setShowPassword(!showPassword)} 
-                style={{filter: showPassword ? 'none' : 'grayscale(100%)'}}></img>
+    return (
+        <div className="loginForm">
+            <div className="header">
+                <h1>Connect with other<br />Make you better</h1>
+                <a href="/"><b>Sign in</b> to your FinnTA Account !</a>
             </div>
-            <div className="Container">
-                <input type="submit" value="Sign in"/> 
-            </div>
-        </form>
-        <p>Don't have an account? <a href="/">Sign up</a></p>
-    </div>
+            <form className="inputForm" onSubmit={handleForm} method="POST">
+                <div className="field">
+                    <input
+                        type="text"
+                        name="ipEmail"
+                        id="ipEmail"
+                        placeholder="avc@avc.com"
+                        onChange={(e) => {
+                            setEmail(e.target.value)
+                        }} required />
+                    <label>Username/Email</label>
+                </div>
+                <div className="field">
+                    <input
+                        type={showPassword ? "text" : "password"}
+                        name="ipPass"
+                        id="ipPass"
+                        placeholder="ThisIsMyPassword"
+                        onChange={(e) => { setPw(e.target.value) }} required />
+                    <label> Password </label>
+                    <img src={showPw} width={24} onClick={() => setShowPassword(!showPassword)}
+                        style={{ filter: showPassword ? 'none' : 'grayscale(100%)' }}></img>
+                </div>
+                <div className="Container">
+                    <input type="submit" value="Sign in" />
+                </div>
+            </form>
+            <p>Don't have an account? <a href="/">Sign up</a></p>
+        </div>
     );
 }
 export default Login;
