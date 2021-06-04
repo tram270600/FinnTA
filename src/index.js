f7be43f6915fd9d536e064759bdba0266a7a9220
= <=>
2021-06-04 16:07:40 +0700

    [Fix] Missing import index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from 'app/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
